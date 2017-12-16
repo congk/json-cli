486a8c330e56262192a68fce60e393a010e3c26a
coderk <363959579@qq.com>
2017-12-17 01:31:35 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const program = require('commander');
-const methods = ['verify', 'format', 'uglify', 'edit'];
+const methods = ['verify', 'format', 'uglify', 'edit', 'make'];
 const trace = require('./lib/util/trace');
 const fs = require('fs');
 
@@ -9,9 +9,12 @@ program
     .version('1.0.0')
     .usage('[options] <path>')
     .option('-v, --verify <path>', '验证json文件格式是否正确')
-    .option('-e, --edit <path>', '编辑json文件，若文件不存在则在指定路径下新建')
+    .option('-m --make <path>', '生成json文件')
+    .option('-t, --type [rootType]', '指定生成json文件的根节点类型，可用类型包括\'obj\'或\'arr\'', 'obj')
+    .option('-e, --edit <path>', '编辑json文件')
     .option('-f, --format <path>', '格式化json文件并保存')
     .option('-u, --uglify <path>', '压缩json文件并保存');
+
 
 program.parse(process.argv);
 
@@ -21,21 +24,23 @@ const done = methods.some((method) => {
         return false;
     }
     const exits = fs.existsSync(path);
-    if (!exits && method !== 'edit') {
+    const isMake = method === 'make';
+
+    if (!exits && !isMake) {
         trace(`No such file on the path: ${path}`);
-        return true;
-    }
-    let content = exits ? fs.readFileSync(path, 'utf8') : "{}";
-    try {
-        content = JSON.parse(content);
-        require(`./lib/method/${method}`)(content, path);
-        trace('success');
-    } catch (err) {
-        trace(err);
+    } else if (exits && isMake) {
+        trace(`File exists on the path: ${path}`);
+    } else if (isMake) {
+        trace(`make file type: ${program.type}`);
+    } else {
+        try {
+            require(`./lib/method/${method}`)(JSON.parse(fs.readFileSync(path, 'utf8')), path);
+        } catch (err) {
+            trace(err);
+        }
     }
     return true;
 });
-
 if (!done) {
     program.outputHelp();
 }
